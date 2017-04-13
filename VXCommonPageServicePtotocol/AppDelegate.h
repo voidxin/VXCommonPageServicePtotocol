a91d1de5deb959ebb0a4c0156fab85218932e42f
voidxin <zhangxin409999@163.com>
2017-04-13 09:35:21 +0800

    Initial Commit

---
+++ VXCommonPageServicePtotocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VXCommonPageServicePtotocol
+//
+//  Created by voidxin on 17/4/13.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
